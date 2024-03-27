b3c4eb31b2ac2d238764b6b3298b3fa271bd5f68
T-Thirunilainayagi <84350468+T-Thirunilainayagi@users.noreply.github.com>
2024-03-27 09:55:30 +0530

    Add files via upload

---
+++ Assignment/src/AssignmentQuest3/StringSearch.java
@@ -0,0 +1,12 @@
+package AssignmentQuest3;
+
+public class StringSearch extends Abstractsearch{
+
+	@Override
+	public boolean search(Object[] obj_list, Object obj) {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+}
